564e2dbae9409939b76a7c47e74153792e165b84
lvs <lvs.freelance@gmail.com>
2016-10-09 10:35:17 +0700

    First commit

--- app/src/main/java/com/lvsandroid/parseserverclient/MainActivity.java
+++ app/src/main/java/com/lvsandroid/parseserverclient/MainActivity.java
@@ -4,13 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.parse.FindCallback;
+import com.parse.GetCallback;
 import com.parse.Parse;
 import com.parse.ParseACL;
 import com.parse.ParseAnalytics;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,19 +24,85 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        // == INSERT
+//        ParseObject score = new ParseObject("Score");
+//        score.put("username","donald");
+//        score.put("score",960);
+//        score.saveInBackground(new SaveCallback() {
+//            @Override
+//            public void done(ParseException e) {
+//                if (e == null) {
+//                    Log.i("saveInBackground", "Successful");
+//                } else {
+//                    Log.e("saveInBackground", "Failed");
+//                    Log.e("saveInBackground",e.toString());
+//                }
+//            }
+//        });
+
+//        // == UPDATE
+//        ParseQuery<ParseObject> query = ParseQuery.getQuery("Score");
+//        query.getInBackground("exA3wnLjdh", new GetCallback<ParseObject>() {
+//            @Override
+//            public void done(ParseObject object, ParseException e) {
+//                object.put("score", 212);
+//                object.saveInBackground(new SaveCallback() {
+//                    @Override
+//                    public void done(ParseException e) {
+//                        if (e == null) {
+//                            Log.i("saveInBackground", "Successful");
+//                        } else {
+//                            Log.e("saveInBackground", "Failed");
+//                            Log.e("saveInBackground", e.toString());
+//                        }
+//                    }
+//                });
+//            }
+//        });
+
+//        // == SELECT ALL
+//        ParseQuery<ParseObject> query = ParseQuery.getQuery("Score");
+//
+//        query.findInBackground(new FindCallback<ParseObject>() {
+//            @Override
+//            public void done(List<ParseObject> objects, ParseException e) {
+//                if (e == null) {
+//                    Log.i("findInBackground","Retrieved " + objects.size() + " results");
+//
+//                    for(ParseObject object : objects) {
+//                        Log.i("findInBackground users", String.valueOf(object.get("username")));
+//                    }
+//                } else {
+//                    Log.e("findInBackground", "Failed");
+//                    Log.e("findInBackground", e.toString());
+//                }
+//            }
+//        });
+
+//        // == SELECT SPECIFIC
+//        ParseQuery<ParseObject> query = ParseQuery.getQuery("Score");
+//
+//        query.whereEqualTo("username","donald");
+//
+//        query.findInBackground(new FindCallback<ParseObject>() {
+//            @Override
+//            public void done(List<ParseObject> objects, ParseException e) {
+//                if (e == null) {
+//                    Log.i("findInBackground","Retrieved " + objects.size() + " results");
+//
+//                    for(ParseObject object : objects) {
+//                        Log.i("findInBackground users", String.valueOf(object.get("username")));
+//                    }
+//                } else {
+//                    Log.e("findInBackground", "Failed");
+//                    Log.e("findInBackground", e.toString());
+//                }
+//            }
+//        });
+
+
         ParseAnalytics.trackAppOpenedInBackground(getIntent());
 
-        ParseObject score = new ParseObject("Score");
-        score.put("username","pedro");
-        score.put("score",96);
-        score.saveInBackground(new SaveCallback() {
-            public void done(ParseException e) {
-                if (e == null) {
-                    Log.i("Parse", "Save Succeeded");
-                } else {
-                    Log.e("Parse", "Save Failed");
-                }
-            }
-        });
+
     }
 }
